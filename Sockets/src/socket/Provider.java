0ce9cecb7bde5136b2c637b47a39ba06db8044a0
Vivian Liao <vivian.liao@kp.org>
2017-04-17 16:37:56 -0700

    can return multiple lines (ie if command was dir) and prompt for more commands.

--- Sockets/src/socket/Provider.java
+++ Sockets/src/socket/Provider.java
@@ -6,8 +6,8 @@ public class Provider {
 	ServerSocket providerSocket;
 	int portNumber=9999;
 	Socket connection=null;
-	ObjectOutputStream out;
-	ObjectInputStream in;
+	PrintWriter out;
+	BufferedReader in;
 	String message;
 	
 	void run(){
@@ -18,16 +18,20 @@ public class Provider {
 			System.out.println("Waiting for connection");
 			connection = providerSocket.accept();
 			//3.get I/O streams
-			out = new ObjectOutputStream(connection.getOutputStream());
+			out = new PrintWriter(connection.getOutputStream(),true);
 			out.flush();
-			in = new ObjectInputStream(connection.getInputStream());
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			sendMessage("Connection Successful");
 			//4. communicate with client via IO stream
+			//initial hello sequence
+			message=(String)in.readLine();
+			System.out.println("client> " +message);
 			do {
-				try{
-					message=(String)in.readObject();
+					//now communication loop, executing commands 
+					message=(String)in.readLine();
 					System.out.println("client> " +message);
-					if (message.equals("list")) {
+					
+					/*if (message.equals("list")) { //or whatever your cue is to execute command line commands on this server
 						System.out.println("executing a command");
 						ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "cd");
 						builder.redirectErrorStream(true);
@@ -35,22 +39,36 @@ public class Provider {
 						BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
 						String line;
 						 while ((line=r.readLine())!=null){
-					            System.out.println(line);
+					            sendMessage(line);
+					            
 					        }
 						r.close();
-					}
-					if (message.equals("bye")){
+					}*/
+					if (message.equals("bye")){ //if client says bye, respond bye as well. 
 						sendMessage("bye");
 					}
-				}catch(ClassNotFoundException classnot){
-					System.err.println("Data received in unknown format");
-				}
+					
+					//either bye or a command
+					else {
+							System.out.println("executing a command");
+							ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", message);
+							builder.redirectErrorStream(true);
+							java.lang.Process p = builder.start(); 
+							BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+							String line;
+							while ((line=r.readLine())!=null){
+						            sendMessage(line);
+						        }
+							 r.close();
+							 out.println("alldone");
+							 System.out.println("done sending result");
+					}
+	
 			}while(!message.equals("bye"));
 				
 		} catch (IOException e){
 			System.out.println("Exception caught when trying to listen on port " + portNumber + 
 					" or listening for a connection");
-			System.out.println(e.getMessage());
 		}
 		finally{
 			try{
@@ -64,13 +82,9 @@ public class Provider {
 		}
 	}	
 	void sendMessage(String msg){
-		try{
-			out.writeObject(msg);
-			out.flush();
-			System.out.println("server> " + msg);
-		} catch(IOException e){
-			e.printStackTrace();
-		}
+		out.println(msg); //send to client, sits in clients inputstream, read with in.readLine
+		out.flush();
+		System.out.println("server> " + msg);	
 	}
 	public static void main(String args[]){
 		Provider server = new Provider();
@@ -80,8 +94,10 @@ public class Provider {
 	}
 	
 }
-/*have java communicate with the shell, ie client says ls -l, need server side to run ls- l, return restults. 
+/*have java communicate with the shell, ie client says ls -l, need server side to run ls- l, return results. 
 		run EchoServer as a service, just listenin. not always running, jsut waiting for a command on the port 
+	
+	try java service wrapper
 	
 		*
 client server basic app copied from http://stackoverflow.com/questions/1776457/java-client-server-application-with-sockets */
--- Sockets/src/socket/Requester.java
+++ Sockets/src/socket/Requester.java
@@ -19,22 +19,26 @@ public class Requester {
 			out.flush();
 			in = new BufferedReader(new InputStreamReader(requestSocket.getInputStream()));
 			stdIn = new BufferedReader(new InputStreamReader(System.in));
-			//3. communicate with server
+			// 3. communicate with server
+			server_message = (String)in.readLine(); 
+			System.out.println("server> "+server_message);
+			sendMessage("Hello server");
+				
+			
 			do{
-				server_message = (String)in.readLine(); 
-				System.out.println("server> "+server_message);
-				sendMessage("Hello server");
 				
-				//figure out how this process should look, keep asking for more commands until bye? 
-				//for now, just accepts one command. maybe require to preface it with cmd.exe 
+				//keep asking for more commands until bye? 
 				System.out.println("what would you like to do");
-				String inputline = stdIn.readLine();
-				sendMessage(inputline);
-				
-				//sendMessage("list");
-				server_message = "bye";
-				sendMessage(server_message);
+				server_message = stdIn.readLine();  //input from client
+				sendMessage(server_message); //send it to server
+				String result;
+				while ((result=in.readLine())!=null && !result.equals("alldone")){  
+					System.out.println("server1> " + result);
+				}
+				System.out.println("done printing results");
 			}while(!server_message.equals("bye"));
+			
+			
 		}catch(UnknownHostException unknownHost){
 			System.err.println("unknown host");
 		}catch(IOException e){
@@ -51,7 +55,7 @@ public class Requester {
 		}
 	}
 	void sendMessage(String msg){
-		out.println(msg); //this sends the message to the server i think
+		out.println(msg); //this sends the message to the server
 		out.flush();
 		System.out.println("client1> "+msg); //this prints to our client command window
 	}
