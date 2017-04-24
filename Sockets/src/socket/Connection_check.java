c348fb5072cc69480b3e14ee451a99e1810932f0
vivian <vvnliao@gmail.com>
2017-04-24 16:12:44 -0700

    basic ssh connection with private key success

--- Sockets/src/socket/Connection_check.java
+++ Sockets/src/socket/Connection_check.java
@@ -1,10 +1,39 @@
 package socket;
-import com.jcraft.jsch.ChannelSftp;
+import javax.swing.*;
+import java.awt.*;
+import com.jcraft.jsch.*;
+
 public class Connection_check {
+String user;
+String host;
+int port;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	void run(String args[]) {
+		user=args[0];
+		host=args[1];
+		port=22;
+		
+		try{
+			JSch jsch= new JSch();
+			jsch.addIdentity("~/.ssh/id_rsa");
+			Session session=jsch.getSession(user,host,port);
+			session.setConfig("PreferredAuthentications", "publickey");
+			session.setConfig("StrictHostKeyChecking", "no");
+			System.out.println("Establishing Connection to "+user + "@"+host);
+			session.connect();
+			System.out.println("connection established");
+			session.disconnect();
+			
+		}
+		catch(Exception e){
+			e.printStackTrace(System.out);
+			//update db with failure. try a text file for now. 
+		}
+		
 	}
-
+	public static void main(String args[]){
+		Connection_check tryit = new Connection_check();
+	    String arglist[] = {"vivian", "192.168.128.135"};
+		tryit.run(arglist);
+	}
 }
